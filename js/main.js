1203a718ee3c35da1ae5f9eab664c09602c65cc9
Sasha Ahrens <>
2021-09-13 17:27:10 -0400

    adding responsive design, finishing home page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// BELOW IS THE CODE FOR THE MOBILE NAV, DO NOT BREAK
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
